1f2fbb514f927670e18141bdd4fc17ce0e18694d
Dimitri Molenaars <Tyrope@tyrope.nl>
2018-05-26 12:49:40 +0100

    Garbage collection for dead creeps

--- main.js
+++ main.js
@@ -3,10 +3,31 @@ var logVerbose = true;
 var SpawnController = require('spawnController');
 var RoleController = require('roleController');
 
+var garbageCollect = function(){
+    if(logVerbose){
+        console.log('main::garbageCollect::start');
+    }
+    for(let mem in Memory.creeps){
+        if(!Game.creeps[mem]){
+            delete Memory.creeps[mem];
+            if(logVerbose){
+                console.log('main::garbageCollect::deleted '+mem);
+            }
+        }
+    }
+    if(logVerbose){
+        console.log('main::garbageCollect::end');
+    }
+}
+
 module.exports.loop = function() {
     if(logVerbose){
         console.log('main::start');
     }
+
+    // Memory cleanup.
+    garbageCollect();
+
     // Do we need to spawn a new creep?
     SpawnController.DoSpawns();
 
