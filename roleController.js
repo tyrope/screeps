967563622ae34e192de6f2ba6687e2267029bcd9
Dimitri Molenaars <Tyrope@tyrope.nl>
2018-05-27 19:48:24 +0100

    Don't catch such a generic thing. Let the program scream a little when things go wrong. Also don't control creeps that are still spawning.

--- roleController.js
+++ roleController.js
@@ -15,15 +15,24 @@ module.exports = {
 
         // Tick each creep.
         for(let i in Game.creeps){
-            let creep = Game.creeps[i]
-            try{
-                Roles[creep.memory.role].Tick(creep);
-                if(logVerbose){
-                    console.log('RoleController::Controlling: '+creep.name);
-                }
-            }catch(TypeError){
-                console.log('RoleController::Unknown role: '+creep.name);
+            let creep = Game.creeps[i];
+
+            // This even a role we know?
+            if(creep.memory.role in Roles == false){
+                console.log('RoleController::Unknown role: '+creep.memory.role);
+                continue;
             }
+
+            // If this creep is still spawning, don't control it.
+            if(creep.spawning){
+                continue;
+            }
+
+            // Alright, we know of it's role and it's done spawning.
+            if(logVerbose){
+                console.log('RoleController::Controlling: '+creep.name);
+            }
+            Roles[creep.memory.role].Tick(creep);
         }
 
         if(logVerbose){
