f02c184ad2ff47e0b6538bee37ef9898e86b9aaf
Dimitri Molenaars <Tyrope@tyrope.nl>
2018-05-27 19:50:13 +0100

    Verbose logging, should it be required.

--- role.cub.js
+++ role.cub.js
@@ -1,3 +1,5 @@
+var logVerbose = false;
+
 var Config = require('config');
 var Pathfinding = require('pathfinding');
 
@@ -14,6 +16,9 @@ module.exports = {
     Tick: function(creep){
         // We moving?
         if(creep.memory.activePath.length){
+            if(logVerbose){
+                console.log('role.cub::Tick::Continuing move.');
+            }
             //Yup, so move.
             let loc = creep.memory.activePath[0];
             let mv = creep.move(creep.pos.getDirectionTo(loc.x,loc.y));
@@ -28,9 +33,15 @@ module.exports = {
 
         // We full?
         if(creep.carry.energy < creep.carryCapacity){
+            if(logVerbose){
+                console.log('role.cub::Tick::Storage not full.');
+            }
             //Nope, go fetch.
             let sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE){
+                if(logVerbose){
+                    console.log('role.cub::Tick::Moving to Source.');
+                }
                 // Not close enough, go there.
                 creep.memory.activePath = Pathfinding.findPath(
                     creep.pos,
@@ -42,12 +53,18 @@ module.exports = {
             }
         }else{
             // Find a place that needs energy.
+            if(logVerbose){
+                console.log('role.cub::Tick::Storage full.');
+            }
             let targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return structure.energy < structure.energyCapacity
                 }
             });
             if(targets.length){
+                if(logVerbose){
+                    console.log('role.cub::Tick::Deposit destination located.');
+                }
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.memory.activePath = Pathfinding.findPath(
                         creep.pos,
