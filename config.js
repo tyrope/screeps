de273d1567224a80dfd69d11c634389acd062a43
Dimitri Molenaars <Tyrope@tyrope.nl>
2018-05-27 16:17:44 +0100

    Refactor config to allow it to self-reference. Also futureproof IdleArea so it works cross-room.

--- config.js
+++ config.js
@@ -1,9 +1,18 @@
+// USER CONFIGURABLE.
+var DesiredCreeps = {'Cub': 1, 'Sarafina': 2, 'Simba': 1, 'Scar': 0};
+var SpawnName = 'LionsDen';
+var IdlePosition = {x: 20, y:20};
+
+/* END OF CONFIGURATION. DO NOT TOUCH BELOW THIS LINE.
+ * **********************************
+ */
 module.exports = {
-    DesiredCreeps: { 'Cub':1, 'Sarafina':2, 'Simba':1, 'Scar': 0 },
-    SpawnName: 'LionsDen',
+    DesiredCreeps: DesiredCreeps,
+    SpawnName: SpawnName,
     IdleArea: {
-        x:20,
-        y:20
+        roomName: Game.spawns[SpawnName].room.name,
+        x:IdlePosition.x,
+        y:IdlePosition.y
     }
 }
 
