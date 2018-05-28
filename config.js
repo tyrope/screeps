2362914bcb2d78f97d74b518068b5db831d35996
Dimitri Molenaars <Tyrope@tyrope.nl>
2018-05-28 01:20:51 +0100

    Configurable mining source, because sometimes 0 isn't the one you want.

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 var DesiredCreeps = {'Cub': 1, 'Sarafina': 2, 'Simba': 1, 'Scar': 0};
 var SpawnName = 'LionsDen';
 var IdlePosition = {x: 20, y:20};
+var preferredSource = 1;
 
 /* END OF CONFIGURATION. DO NOT TOUCH BELOW THIS LINE.
  * **********************************
@@ -9,6 +10,7 @@ var IdlePosition = {x: 20, y:20};
 module.exports = {
     DesiredCreeps: DesiredCreeps,
     SpawnName: SpawnName,
-    IdleArea: new RoomPosition(IdlePosition.x, IdlePosition.y, Game.spawns[SpawnName].room.name)
+    IdleArea: new RoomPosition(IdlePosition.x, IdlePosition.y, Game.spawns[SpawnName].room.name),
+    Source: preferredSource
 }
 
