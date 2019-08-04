fc5c8e36a8f896ee38a582bb93d7b5c25074b3dd
Dimitri Molenaars <tyrope@tyrope.nl>
2019-08-04 23:30:31 +0100

    MOAR CREEPS! Also, this LICENSE.js keeps popping up, might as well put it on git then.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // USER CONFIGURABLE.
 
 // The number of creeps you want.
-var DesiredCreeps = {'Cub': 1, 'Simba': 1, 'Sarafina': 1, 'Scar': 0};
+var DesiredCreeps = {'Cub': 2, 'Simba': 5, 'Sarafina': 3, 'Scar': 0};
 
 // The name of the spawn
 var SpawnName = 'Spawn1';
